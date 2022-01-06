ea69396604f893e37a314cbc5ac6b9cd89827063
dzmitry <dmitryzhurkovsky@gmail.com>
2022-01-06 19:57:20 +0300

    Divide Templates, add statics files, add urls

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,10 @@
-import sys, os 
-cwd = os.getcwd() 
-sys.path.append(cwd) 
-sys.path.append(cwd + '/mysite') 
-os.environ['DJANGO_SETTINGS_MODULE'] = "seventrans.settings" 
-from django.core.wsgi import get_wsgi_application 
+import os
+import sys
+
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + '/mysite')
+os.environ['DJANGO_SETTINGS_MODULE'] = "seventrans.settings"
+from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
