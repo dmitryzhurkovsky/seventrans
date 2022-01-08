1f0c2a5bc16ac3417840258afe3768200a899efa
dzmitry <dmitryzhurkovsky@gmail.com>
2022-01-08 16:48:55 +0300

    Change Article model

--- news/models.py
+++ news/models.py
@@ -5,13 +5,13 @@ class Article(models.Model):
     class Meta:
         ordering = ('-publish_date',)
 
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=255)
     publish_date = models.DateField()
 
     body = models.TextField()
-    preview_body = models.CharField(max_length=250)
+    preview_body = models.CharField(max_length=255)
 
-    img_url = models.CharField(max_length=125, blank=True, null=True)
+    img_url = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.title
