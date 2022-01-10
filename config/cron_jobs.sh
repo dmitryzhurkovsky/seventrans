62fcfa64baf3231629cebb2a78ef7ae1e20961a0
dzmitry <dmitryzhurkovsky@gmail.com>
2022-01-10 22:40:23 +0300

    Fix cron

--- config/cron_jobs.sh
+++ config/cron_jobs.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 source /home/a7transb/virtualenv/repositories/seventrans/3.9/bin/activate &&
  cd /home/a7transb/repositories/seventrans &&
  python manage.py shell < config/cron_jobs.py
