5504a219f25512f1fe27b42043127d4203c0b4ab
dzmitry <dmitryzhurkovsky@gmail.com>
2022-01-10 23:37:55 +0300

    Fix templates

--- pages/urls.py
+++ pages/urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 
 from pages.views import (
     IndexView,
-    AboutView
+    AboutView,
+    NewsView,
+    ContactsView,
+    ServicesView
 )
 
 urlpatterns = [
     path('', IndexView.as_view()),
-    path('about/', AboutView.as_view())
+    path('about/', AboutView.as_view()),
+    path('news/', NewsView.as_view()),
+    path('contacts/', ContactsView.as_view()),
+    path('services/', ServicesView.as_view()),
+
 ]
