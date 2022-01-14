05251c6a15ec1fafa44553cb74eef9b1d519cca8
dzmitry <dmitryzhurkovsky@gmail.com>
2022-01-14 19:37:36 +0300

    Add vue file in static folder

--- static/js/vue.js
+++ static/js/vue.js
@@ -7,17 +7,40 @@ const app = Vue.createApp(config)
 app.component('lagnuage-tumbler', {
     data() {
         return {
-            language: 'ru'
+            language: this.getCookie('django_language'),
         }
     },
     methods: {
-        changeLanguage (language) {
+        getCookie(cookieName) {
+            let name = cookieName + "=";
+            let decodedCookie = decodeURIComponent(document.cookie);
+            let ca = decodedCookie.split(';');
+            for (let i = 0; i < ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return "";
+        },
+        setCookie(cookieName, cookieValue, exDays) {
+            const d = new Date();
+            d.setTime(d.getTime() + (exDays * 24 * 60 * 60 * 1000));
+            let expires = "expires=" + d.toUTCString();
+            document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+        },
+        changeLanguage(language) {
             this.language = language
-            console.log(language)
+            this.setCookie('django_language', language, 1);
+            this.setCookie('language', language, 1);
+            location.reload();
         }
     },
     template:
-    `
+        `
     <div class="header__language language _flex">
         <div class="language__box">
             <div
