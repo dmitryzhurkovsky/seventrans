15162fa9365ff149f5db0b75930013e5d25b9194
dzmitry <dmitryzhurkovsky@gmail.com>
2022-01-23 19:46:37 +0300

    Fix restriction length

--- pages/utils.py
+++ pages/utils.py
@@ -11,9 +11,9 @@ def modify_len_title_and_body_of_news(news: QuerySet) -> list:
     first_article['title'] = first_article.get('title')[:settings.NEWS_INDEX_PAGE_FIRST_ARTICLE_TITLE_LENGTH]
 
     second_article['title'] = second_article.get('title')[:settings.NEWS_INDEX_PAGE_SECOND_ARTICLE_TITLE_LENGTH]
-    second_article['body'] = second_article.get('body')[:settings.NEWS_INDEX_PAGE_SECOND_ARTICLE_BODY_LENGTH]
+    second_article['preview_body'] = second_article.get('body')[:settings.NEWS_INDEX_PAGE_SECOND_ARTICLE_PREVIEW_BODY_LENGTH]
 
     third_article['title'] = third_article.get('title')[:settings.NEWS_INDEX_PAGE_THIRD_ARTICLE_TITLE_LENGTH]
-    third_article['body'] = third_article.get('body')[:settings.NEWS_INDEX_PAGE_THIRD_ARTICLE_BODY_LENGTH]
+    third_article['preview_body'] = third_article.get('body')[:settings.NEWS_INDEX_PAGE_THIRD_ARTICLE_PREVIEW_BODY_LENGTH]
 
     return [first_article, second_article, third_article]
