7e32d3af1e7617a288d4e85d6a602b9605d5fbbd
dzmitry <dmitryzhurkovsky@gmail.com>
2022-01-24 13:57:15 +0300

    Fix MEDIA ROOT

--- config/settings.py
+++ config/settings.py
@@ -110,7 +110,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = 'https://7trans.by/public_html/media/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'config', 'static/')]
 
