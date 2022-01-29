b7c9e27685eec0b7174f21e78c238a116a25d326
dzmitry <dmitryzhurkovsky@gmail.com>
2022-01-29 13:47:37 +0300

    Refactoring language changing component

--- static/js/components/languageTumbler.js
+++ static/js/components/languageTumbler.js
@@ -1,50 +1,22 @@
 languageTumblerComponent = {
-    data() {
-        return {
-            language: this.getCookie('django_language') ? this.getCookie('django_language') : 'ru',
-        }
-    },
     methods: {
-        changeLanguage(language) {
-            this.language = language
-            this.setCookie('django_language', language, 1);
-            this.setCookie('language', language, 1);
-            location.reload();
-        },
-        getCookie(cookieName) {
-            let name = cookieName + "=";
-            let decodedCookie = decodeURIComponent(document.cookie);
-            let ca = decodedCookie.split(';');
-            for (let i = 0; i < ca.length; i++) {
-                let c = ca[i];
-                while (c.charAt(0) == ' ') {
-                    c = c.substring(1);
-                }
-                if (c.indexOf(name) == 0) {
-                    return c.substring(name.length, c.length);
-                }
-            }
-            return "";
-        },
-        setCookie(cookieName, cookieValue, exDays) {
-            const d = new Date();
-            d.setTime(d.getTime() + (exDays * 24 * 60 * 60 * 1000));
-            let expires = "expires=" + d.toUTCString();
-            document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
-        },
+      changeLanguage(language) {
+          this.$emit('changeLanguage', language)
+      }
     },
+    props: ['language'],
     template:
         `
     <div class="header__language language _flex">
         <div class="language__box">
             <div
-                    @click="changeLanguage('ru')"
-                    :class="{'_active': this.language === 'ru'}"
+                @click="changeLanguage('ru')"
+                    :class="{'_active': language === 'ru'}"
                     class="language__item _flex"
             >RU</div>
             <div
                     @click="changeLanguage('en')"
-                    :class="{'_active': this.language === 'en'}"
+                    :class="{'_active': language === 'en'}"
                     class="language__item _flex"
             >EN</div>
         </div>
--- static/js/vue.js
+++ static/js/vue.js
@@ -1,4 +1,9 @@
 const config = {
+    data() {
+        return {
+            language: this.getCookie('django_language') ? this.getCookie('django_language') : this.changeLanguage('ru')
+        }
+    },
     delimiters: ["[[", "]]"],
     methods: {
         redirectToPage(endPoint) {
@@ -7,6 +12,33 @@ const config = {
             } else if (endPoint === 'about') {
                 window.open(`https://7trans.by/about`);
             }
+        },
+        getCookie(cookieName) {
+            let name = cookieName + "=";
+            let decodedCookie = decodeURIComponent(document.cookie);
+            let ca = decodedCookie.split(';');
+            for (let i = 0; i < ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return "";
+        },
+        setCookie(cookieName, cookieValue, exDays) {
+            const d = new Date();
+            d.setTime(d.getTime() + (exDays * 24 * 60 * 60 * 1000));
+            let expires = "expires=" + d.toUTCString();
+            document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+        },
+        changeLanguage(language) {
+            this.language = language
+            this.setCookie('django_language', language, 1);
+            this.setCookie('language', language, 1);
+            location.reload();
         }
     }
 }
