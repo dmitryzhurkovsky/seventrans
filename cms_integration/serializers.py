29e5d0d8f87a3897a04c30159ceb820711f439f1
dzmitry <dmitryzhurkovsky@gmail.com>
2022-01-30 15:55:05 +0300

    Add preview for services

--- cms_integration/serializers.py
+++ cms_integration/serializers.py
@@ -6,7 +6,7 @@ from cms_integration.models import Service
 
 class ServiceSerializer(serializers.ModelSerializer):
     title = serializers.SerializerMethodField()
-    content = serializers.SerializerMethodField()
+    preview = serializers.SerializerMethodField()
 
     class Meta:
         model = Service
@@ -14,19 +14,11 @@ class ServiceSerializer(serializers.ModelSerializer):
             'id',
             'title',
             'img',
-            'content',
+            'preview',
         )
 
     def get_language(self):
         return self.context['request'].headers.get('Accept-Language')
-
-    def get_content(self, instance):
-        language = self.get_language()
-
-        if language == 'ru':
-            return instance.content_1_ru[:settings.SERVICES_PAGE_SERVICE_CONTENT_LENGTH]
-
-        return instance.content_1_en[:settings.SERVICES_PAGE_SERVICE_CONTENT_LENGTH]
 
     def get_title(self, instance):
         language = self.get_language()
@@ -35,3 +27,11 @@ class ServiceSerializer(serializers.ModelSerializer):
             return instance.title_ru
 
         return instance.title_en
+
+    def get_preview(self, instance):
+        language = self.get_language()
+
+        if language == 'ru':
+            return instance.preview_ru
+
+        return instance.preview_en
