0e31d5fb723f2b7e4679410fb5d44b21f596d4df
dzmitry <dmitryzhurkovsky@gmail.com>
2022-01-30 22:16:50 +0300

    Change path

--- config/settings.py
+++ config/settings.py
@@ -111,7 +111,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_ROOT = 'https://7trans.by/public_html/media/'
+MEDIA_ROOT = '/home/a7transb/public_html/media/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'config', 'static/')]
 
